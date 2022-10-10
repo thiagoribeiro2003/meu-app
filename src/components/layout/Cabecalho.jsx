dd7a2dbb437fe41c61734cda5ae7b826d2c3be41
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2022-10-10 11:45:20 -0300

    CSS inline

--- src/components/layout/Cabecalho.jsx
+++ src/components/layout/Cabecalho.jsx
@@ -1,7 +1,8 @@
 import Menu from "../Menu";
 const Cabecalho = () => {
   return (
-    <header>
+    /* CSS IN JS */
+    <header style={{ backgroundColor: "lightblue", textAlign: "center" }}>
       <h1>Interface React</h1>
       <Menu />
     </header>
