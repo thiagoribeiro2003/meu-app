6e94d55accae9597c022d00a23173ef34925dda2
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2022-10-10 11:53:23 -0300

    CSS inline e interno

--- src/components/layout/Cabecalho.jsx
+++ src/components/layout/Cabecalho.jsx
@@ -1,9 +1,18 @@
 import Menu from "../Menu";
+
+/* Objeto de estilos CSS */
+const estilos = {
+  color: "white",
+  backgroundColor: "black",
+};
+
 const Cabecalho = () => {
   return (
-    /* CSS IN JS */
+    /* CSS IN JS: estilos CSS embutidos no meio DO JavaScript*/
+
+    /* CSS inline (usa propriedade style e sintaxe de objeto)*/
     <header style={{ backgroundColor: "lightblue", textAlign: "center" }}>
-      <h1>Interface React</h1>
+      <h1 style={estilos}>Interface React</h1>
       <Menu />
     </header>
   );
