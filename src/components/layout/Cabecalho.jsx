71a2058baea61c126e73b6b41a1c5e6f8dcb9dd0
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2022-10-11 11:58:42 -0300

    testando bootstrap no react

--- src/components/layout/Cabecalho.jsx
+++ src/components/layout/Cabecalho.jsx
@@ -1,7 +1,10 @@
 import Menu from "../Menu";
+import Button from "react-bootstrap/Button";
+
 const Cabecalho = () => {
   return (
     <header>
+      <Button variant="danger">Teste</Button>
       <h1>Interface React</h1>
       <Menu />
     </header>
