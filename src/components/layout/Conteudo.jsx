361f8c69fff4331b69b26b4382c091cb76d52671
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2022-10-11 08:23:54 -0300

    Update Conteudo.jsx

--- src/components/layout/Conteudo.jsx
+++ src/components/layout/Conteudo.jsx
@@ -1,8 +1,11 @@
+// Comentário JS ctrl ;
+/* Comentário JS shift alt a */
 import Artigo from "../Artigo";
 const Conteudo = () => {
   return (
     <main>
       <section>
+        {/*Aqui é um comentário dentro do jsx*/}
         <h2>Conteúdo do site.....</h2>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea
