8ba4f06b33b6c518789ffe19cc6cd5d8d308a72b
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2022-10-11 11:28:00 -0300

    Instalação e teste do styled components

--- src/components/layout/Cabecalho.jsx
+++ src/components/layout/Cabecalho.jsx
@@ -1,10 +1,36 @@
+import styled from "styled-components";
 import Menu from "../Menu";
+
+const periodo = "noturno";
+
+/* Tagged Template para uso com styled components */
+const StyledCabecalho = styled.header`
+  background-color: ${periodo === "noturno" ? "lightblue" : "darkblue"};
+  text-align: center;
+
+  h1 {
+    color: red;
+  }
+
+  p {
+    font-size: 2rem;
+    font-weight: bold;
+    color: blue;
+
+    &:hover {
+      color: red;
+      cursor: pointer;
+    }
+  }
+`;
+
 const Cabecalho = () => {
   return (
-    <header>
+    <StyledCabecalho>
       <h1>Interface React</h1>
       <Menu />
-    </header>
+      <p>Teste</p>
+    </StyledCabecalho>
   );
 };
 
