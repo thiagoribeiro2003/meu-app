316ec245ad8988dba631155ac74ca1cc638cd26d
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2022-10-13 10:41:15 -0300

    Eventos exemplo simples

--- src/components/layout/Cabecalho.jsx
+++ src/components/layout/Cabecalho.jsx
@@ -1,7 +1,14 @@
 import Menu from "../Menu";
 const Cabecalho = () => {
+  const exemplo2 = () => {
+    console.log("Exemplo2");
+  };
+
   return (
     <header>
+      <button onClick={() => console.log("Exemplo1")}>Exemplo 1</button>
+      <button onClick={exemplo2}>Exemplo 2</button>
+      <hr />
       <h1>Interface React</h1>
       <Menu />
     </header>
