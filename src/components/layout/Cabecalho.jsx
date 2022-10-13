30c08721318e5678c603bf299a3ba125767652ca
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2022-10-13 11:07:22 -0300

    usando useState para eventos

--- src/components/layout/Cabecalho.jsx
+++ src/components/layout/Cabecalho.jsx
@@ -1,7 +1,18 @@
+import { useState } from "react";
 import Menu from "../Menu";
 const Cabecalho = () => {
   const exemplo2 = () => {
     console.log("Exemplo2");
+  };
+
+  /* Exemplo 1 de gerenciamento de states
+  Aqui, definimos no states um valor inicial (colocando entre os parênteses do 
+  useState), além de desestruturar o state em uma variável (titulo) e em uma 
+  função (setTitulo) que será responsável por atualizar este state/título */
+  const [titulo, setTitulo] = useState("Exemplo de State");
+
+  const atualizaTitulo = () => {
+    setTitulo("Opa, state do titulo foi alterado!");
   };
 
   return (
@@ -9,7 +20,9 @@ const Cabecalho = () => {
       <button onClick={() => console.log("Exemplo1")}>Exemplo 1</button>
       <button onClick={exemplo2}>Exemplo 2</button>
       <hr />
-      <h1>Interface React</h1>
+
+      {/* Aqui, aplicamos a variável de state chamada titulo */}
+      <h1 onClick={atualizaTitulo}> {titulo} </h1>
       <Menu />
     </header>
   );
