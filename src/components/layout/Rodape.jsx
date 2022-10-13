72461864cf2c4e9cb8d576b44cdabe3f51f897e4
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2022-10-13 11:50:07 -0300

    Formatação do rodapé função de exibir e esconder

--- src/components/layout/Rodape.jsx
+++ src/components/layout/Rodape.jsx
@@ -1,8 +1,28 @@
+import { useState } from "react";
+
 const Rodape = () => {
+  const [estilos, setEstilos] = useState({
+    textAlign: "Center",
+    textTransform: "uppercase",
+    border: "solid 2px red",
+  });
+
+  const esconder = () => setEstilos({ display: "none" });
+  const exibir = () => setEstilos({ display: "block" });
+
   return (
-    <footer>
-      <h2>Rodapé!</h2>
-    </footer>
+    <>
+      <p>
+        <span onMouseOver={esconder}> Esconder </span> |
+        <span onMouseOver={exibir}> Exibir </span>
+      </p>
+      <footer
+        style={estilos}
+        onMouseOver={() => setEstilos({ display: "none" })}
+      >
+        <h2>Rodapé!</h2>
+      </footer>
+    </>
   );
 };
 export default Rodape;
