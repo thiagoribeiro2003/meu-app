2b22c9bc038d99dfea6eea0507db060e25c94829
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2022-10-13 11:35:06 -0300

    Update Conteudo.jsx

--- src/components/layout/Conteudo.jsx
+++ src/components/layout/Conteudo.jsx
@@ -1,8 +1,24 @@
 import Artigo from "../Artigo";
+import { useState } from "react";
+
 const Conteudo = () => {
+  const [corFundo, setCorFundo] = useState();
+  const [contador, setContador] = useState(0);
+
+  const fundoAmarelo = () => setCorFundo("#FFEE71");
+  const fundoAzul = () => setCorFundo("lightblue");
+  const fundoVermelho = () => setCorFundo("lightpink");
+
+  const atualizarContagem = () => setContador(contador + 1);
+
   return (
-    <main>
+    <main style={{ backgroundColor: corFundo }}>
+      <button onMouseOver={fundoAmarelo}>Amarelo</button>
+      <button onMouseOver={fundoAzul}>Azul</button>
+      <button onMouseOver={fundoVermelho}>Vermelho</button>
+
       <section>
+        <h2 onClick={atualizarContagem}>Contador: {contador}</h2>
         <h2>Conteúdo do site.....</h2>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea
