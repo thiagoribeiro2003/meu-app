f888c2850a6b9fcc922dae6bb29ebef8af22760b
thiagoribeiro2003 <thiagoribeiro20003@gmail.com>
2022-10-14 09:13:46 -0300

    import do react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
 import Cabecalho from "./components/layout/Cabecalho";
 import Rodape from "./components/layout/Rodape";
 import Home from "./pages/Home";
@@ -6,13 +8,24 @@ import Servicos from "./pages/Servicos";
 
 const App = () => {
   return (
-    // Fragmento - tag vazia que serve para agrupar elementos sem ter que fazer uma nova div
     <>
-      <Cabecalho />
-      <Home />
-      <Produtos />
-      <Servicos />
-      <Rodape />
+      {/* Fragmento - tag vazia que serve para agrupar elementos sem ter que fazer uma nova div */}
+      <BrowserRouter>
+        <Cabecalho />
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/produtos">
+            <Produtos />
+          </Route>
+          <Route path="/servicos">
+            <Servicos />
+          </Route>
+        </Switch>
+        <Rodape />
+      </BrowserRouter>
     </>
   );
 };
